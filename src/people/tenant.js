7d0dd389102f94b13e4279b6f3561d90b7d4f50c
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-20 20:48:03 -0700

    Adding tenant js file

--- src/people/tenant.js
+++ src/people/tenant.js
@@ -1,12 +1,18 @@
 var person = require("./person.js");
 
 function Tenant(name, contact) {
-  // set this.name, and contact
+  
+  this.name = name;
+  this.contact = contact;
   this.references = [];
-};
+}
+
+inherits(Person, Tenant);
+
 
 Tenant.prototype.addReference = function(reference){
   // add reference to references
+  this.references.push(reference);
 };
 
 module.exports = Tenant;
