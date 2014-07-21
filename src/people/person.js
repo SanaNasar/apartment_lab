35d78e380d37d178fc3e63866d578ce37829d4fe
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-20 20:47:02 -0700

    Adding person js file

--- src/people/person.js
+++ src/people/person.js
@@ -1,8 +1,17 @@
+
+// Person is the main method which is created with Person function()
+
 function Person(name, contact){
   this.name = name;
   this.contact = contact;
-};
+}
 
+// For testing purpose
+
+// var Person = app.Person;
+// var Sana = new Person("Sana", "123-4567");
+// Sana.contact();
+// Sana.name();
 
 
 module.exports = Person;
