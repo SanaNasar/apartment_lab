a4bc308bdd97601ff6c63a46b3765f63a71ef522
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-20 20:47:33 -0700

    Adding manager js file

--- src/people/manager.js
+++ src/people/manager.js
@@ -1,17 +1,32 @@
 var person = require("./person");
+var inherits = require("./inherits.js");
 
 function Manager(name, contact) {
+
   // set name and contact
+  this.name = name;
+  this.contact = contact;
   
-  this.properties = [];
+  this.properties = []; // Declaring an empty array for properties
 }
+// Manager() inherits the properties from the 'parent function' Person()
+	inherits(Person, Manager);
 
 Manager.prototype.addProperty = function(property) {
-  // add property from properties
+  
+  // addding property from properties
+  this.properties.push(property);
+  //Pushing the property values of Manager() into an empty array.
 };
 
 Manager.prototype.removeProperty = function(property) {
-  // remove properties
+	var removeProperty = this.properties.indexof(property);
+
+	// remove properties
+	// Manager.removeProperty
+
+	var removed = this.properties.splice.removeProperty(1);
+	return removed;
 };
 
 module.exports = Manager;
