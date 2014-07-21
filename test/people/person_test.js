43b8fc6f3faa106ec1488c0c435b0b47512803de
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-20 22:37:25 -0700

    Including test file for person js

--- test/people/person_test.js
+++ test/people/person_test.js
@@ -1,6 +1,22 @@
 var expect = require("chai").expect,
-  Person = require("../src/people/person.js");
+  Person = require("../../src/people/person.js");
+
+  var sana = new Person("sana", "123-456");
 
 describe("Person", function(){
+	describe("initialization", function(){
+		it("should have a name", function(){
+			expect(sana.name).to.exists();
+		});
+
+		it("should have a contact number", function(){
+			expect(sana.contact).to.exists();
+		});
+
+		it("should be an instance of Person", function(){
+			expect(sana instanceof Person).to.eql(true);
+		});
+
+	});
   
 });
